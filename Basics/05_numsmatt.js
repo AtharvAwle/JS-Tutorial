60b3bf734b0984559ab09ebf56f2bb941eb1a389
AtharvAwle <atharvawle0156@gmail.com>
2023-12-26 19:39:43 +0530

    nums section

---
+++ Basics/05_numsmatt.js
@@ -0,0 +1,8 @@
+const score = 400
+const balance = new Number(10000)
+
+//convert number to string
+let balanceString = balance.toString()
+console.log(typeof balanceString); 
+
+//to get precision value use ToFixed(2/3/4/5)
